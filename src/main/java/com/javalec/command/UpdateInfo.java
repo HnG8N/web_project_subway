576bacad876800494c27389ac1942d5b9d7502ea
kimsor1 <imoiksr@gmail.com>
2024-02-15 13:54:01 +0900

    개인정보 수정기능

--- src/main/java/com/javalec/command/UpdateInfo.java
+++ src/main/java/com/javalec/command/UpdateInfo.java
@@ -4,24 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.tomcat.util.digester.SystemPropertySource;
-
 import com.javalec.dao.MyPage_Dao;
-import com.javalec.dto.MyPage_Dto;
 
-public class MyPageCommand implements Command {
+public class UpdateInfo implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-
+		
 		HttpSession session = request.getSession();
-		String id = (String) session.getAttribute("userId");
-
+		String newpw = request.getParameter("newpw"); 
+		String address = request.getParameter("address1")+" "+request.getParameter("address2"); ; 
+		
+		System.out.println(newpw);
+		
 		MyPage_Dao dao = new MyPage_Dao();
-		MyPage_Dto dto = dao.Mypage(id);
-
-		request.setAttribute("dto", dto);
+		dao.updateInfo((String)session.getAttribute("userId"), newpw , address);
 	}
 
 }
