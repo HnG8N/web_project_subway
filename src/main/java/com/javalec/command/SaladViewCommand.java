96d2a0ce37e022c745192c575a623bf9f1b20def
ssujjy <ssujjy@gmail.com>
2024-02-16 09:26:42 +0900

    놋북에서 맥으로 사용하기 위한 커밋

--- src/main/java/com/javalec/command/SaladViewCommand.java
+++ src/main/java/com/javalec/command/SaladViewCommand.java
@@ -3,12 +3,18 @@ package com.javalec.command;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javalec.dao.MenuDao;
+import com.javalec.dto.MenuDto;
+
 public class SaladViewCommand implements Command {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
-
+		int mncode = Integer.parseInt(request.getParameter("menuItemIdx"));
+		MenuDao dao = new MenuDao();
+		MenuDto dto = dao.getMenuDetail(mncode);
+		request.setAttribute("dto", dto);
 	}
 
 }
